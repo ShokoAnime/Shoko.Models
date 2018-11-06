dc00fd17fd22f07101d1aa394d9705c2ab1e7029
Maximo Piva <maximo.piva@gmail.com>
2018-11-06 02:38:01 -0300

    Queue Refactor

--- Client/CL_ServerStatus.cs
+++ Client/CL_ServerStatus.cs
@@ -6,14 +6,17 @@
         public string HashQueueState { get; set; }  //Deprecated since 3.6.0.0
         public int HashQueueStateId { get; set; }
         public string[] HashQueueStateParams { get; set; }
+        public double HashQueueStatePercentage { get; set; }
         public int GeneralQueueCount { get; set; }
         public string GeneralQueueState { get; set; } //Deprecated since 3.6.0.0
         public int GeneralQueueStateId { get; set; }
         public string[] GeneralQueueStateParams { get; set; }
+        public double GeneralQueueStatePercentage { get; set; }
         public int ImagesQueueCount { get; set; }
         public string ImagesQueueState { get; set; } //Deprecated since 3.6.0.0
         public int ImagesQueueStateId { get; set; }
         public string[] ImagesQueueStateParams { get; set; }
+        public double ImagesQueueStatePercentage { get; set; }
         public bool IsBanned { get; set; }
         public string BanReason { get; set; }
         public string BanOrigin { get; set; }
--- Server/ShokoInternal/CommandRequest.cs
+++ Server/ShokoInternal/CommandRequest.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Shoko.Models.Server
 {
     public class CommandRequest
     {
-        public CommandRequest()
-        {
-        }
-        public int CommandRequestID { get; set; }
+    #region Server DB columns
+
+        public string Id { get; set; }
+        public string Class { get; set; }
+        public int Type { get; set; }
+        
+        public string ParallelTag { get; set; }
+        public int ParallelMax { get; set; }
         public int Priority { get; set; }
-        public int CommandType { get; set; }
-        public string CommandID { get; set; }
-        public string CommandDetails { get; set; }
-        public DateTime DateTimeUpdated { get; set; }
+        public string Batch { get; set; }
+        public DateTime ExecutionDate { get; set; }
+        public int Retries { get; set; }
+        public string LastError { get; set; }
+        public string Data { get; set; }
+
+    #endregion
+
     }
 }
