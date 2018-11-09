8147fa94a249968ba17db8548efb523a33c1a826
Maximo Piva <maximo.piva@gmail.com>
2018-11-09 19:47:20 -0300

    Added WebCache Reliability

---
+++ Enums/WebCache_ReliabilityType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shoko.Models.Enums
+{
+    public enum WebCache_ReliabilityType
+    {
+        Verified,
+        User,
+        Popular
+    }
+}
---
+++ WebCache/WebCache_Reliability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shoko.Models.Enums;
+
+namespace Shoko.Models.WebCache
+{
+    public class WebCache_Reliability<T>
+    {
+        public WebCache_ReliabilityType Type { get; set; }
+
+        public T Result { get; set; }
+
+        public int PopularityCount { get; set; }
+    }
+}
