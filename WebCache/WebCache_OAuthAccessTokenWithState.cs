5201faac11b96cc4b1b0faa3f92a34771efc10b9
Maximo Piva <maximo.piva@gmail.com>
2018-11-20 02:21:56 -0300

    Adding ban support

--- WebCache/WebCache_OAuthAccessTokenWithState.cs
+++ WebCache/WebCache_OAuthAccessTokenWithState.cs
@@ -1,8 +1,12 @@
-﻿namespace Shoko.Models.WebCache
+﻿using System;
+
+namespace Shoko.Models.WebCache
 {
     public class WebCache_OAuthAccessTokenWithState : WebCache_OAuthAccessToken
     {
         public string state { get; set; }
+
+        public string error { get; set; }
 
     }
 }
