575b4b1d72520e900fd9ed1fa379dbe0f5b544df
Maximo Piva <maximo.piva@gmail.com>
2018-11-20 13:50:41 -0300

    ef core dont like keys to be on child classes. dope...

--- WebCache/WebCache_FileHash_Collision.cs
+++ WebCache/WebCache_FileHash_Collision.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Shoko.Models.WebCache
 {
-    public class WebCache_FileHash_Collision : WebCache_FileHash_Info
+    public class WebCache_FileHash_Collision
     {
         public int WebCache_FileHash_Collision_Id { get; set; }
         public string WebCache_FileHash_Collision_Unique { get; set; }
+        public int AniDBUserId { get; set; }
+        public bool CollisionApproved { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string ED2K { get; set; }
+        public string CRC32 { get; set; }
+        public string MD5 { get; set; }
+        public string SHA1 { get; set; }
+        public long FileSize { get; set; }
     }
 }
