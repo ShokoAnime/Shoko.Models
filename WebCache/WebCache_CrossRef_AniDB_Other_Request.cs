1eb231f866604b4376f9d6e66970d5472442a98e
Maximo Piva <maximo.piva@gmail.com>
2018-11-21 18:02:39 -0300

    remove obsoletes

--- WebCache/WebCache_CrossRef_AniDB_Other_Request.cs
+++
@@ -1,9 +0,0 @@
-﻿using Shoko.Models.Server;
-
-namespace Shoko.Models.WebCache
-{
-    public class WebCache_CrossRef_AniDB_Other_Request : CrossRef_AniDB_Other
-    {
-        public string Username { get; set; }
-    }
-}
